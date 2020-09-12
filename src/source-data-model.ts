57186b590895c7ea92d8e933aa7ced25473d5ae0
Nimesh Solanki <nimesh3536@gmail.com>
2020-09-12 16:11:38 +0530

    add source-data-model

---
+++ src/source-data-model.ts
@@ -0,0 +1,39 @@
+interface OAuthInfo {
+  provider: string,
+  scope: string,
+}
+
+interface AuthInfo {
+  type: string,
+  oAuth2Info: OAuthInfo
+}
+
+interface Injections {
+  type: string,
+  key: string,
+  interpolationKey: string,
+  label: string,
+  description: string
+}
+
+interface Headers {
+  'Content-Type': string,
+  'developer-token': string,
+  'login-customer-id': string 
+}
+
+interface Connection {
+  type: string,
+  baseUrl: string,
+  headers: Headers,
+  injections: [Injections],
+  authInfo: AuthInfo
+}
+
+export interface SourceData {
+  id: Number,
+  uuid: string,
+  name: string,
+  connectionInfo: Connection
+}
+
