652ce67b9e7b9b3b14c21df086f4b6bcce66e430
Nimesh Solanki <nimesh3536@gmail.com>
2020-09-13 23:46:12 +0530

    use env variables

--- src/api.ts
+++ src/api.ts
@@ -1,9 +1,8 @@
 import axios from "axios";
 import { DataSourceDto } from './types';
 
-const URL = "https://api.airboxr.com/data/dataSources";
-const TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyVXVpZCI6IjUxMzc3OGZmLWQxYWYtNDNjNC1iYzRkLTkzYjRjMmI5YTYwYiIsImlhdCI6MTU5OTgwNjk1MSwiZXhwIjoxNTk5ODEwNTUxfQ.w1d3E-EVywpvttPqYUCAYUIGXduR2CG1mnxPYK-GAZ0";
+const URL = process.env.REACT_APP_URL || '';
+const TOKEN = process.env.REACT_APP_TOKEN || '';
 const config = {
   headers: { Authorization: `Bearer ${TOKEN}` }
 };
