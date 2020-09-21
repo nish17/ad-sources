4ce2ae121c4a491d62f3e654e5df63943ecc0ec6
Nimesh Solanki <nimesh3536@gmail.com>
2020-09-21 18:47:57 +0530

    fix color change

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -2,23 +2,11 @@ import React from 'react'
 import Button from '@material-ui/core/Button';
 import { PageContainer } from "./layout-components";
 import { Typography } from '@material-ui/core';
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
-import {HistoryProp} from './types';
+import { HistoryProp } from './types';
 
-const useStyles = makeStyles((theme: Theme) =>
-createStyles({
-  btn2: {
-    color: 'green',
-    borderColor: 'green',
-    '&:hover': {
-      borderColor: 'green',
-    }
-  }
-})
-);
 
-const Homepage: React.FC<HistoryProp> = ({history}) => {
-  const classes = useStyles();
+
+const Homepage: React.FC<HistoryProp> = ({ history }) => {
 
   const btnClickHandler = () => {
     history.push('/select-source-page')
@@ -28,10 +16,10 @@ const Homepage: React.FC<HistoryProp> = ({history}) => {
     <PageContainer>
       <Typography variant="h2" gutterBottom>What would you like to do today?</Typography>
       <Typography variant="h5" gutterBottom>Welcome to Airboxr. Let's start with the task you want to accomplish today</Typography>
-      <Button variant="outlined" color="secondary" onClick={btnClickHandler}>
+      <Button variant="outlined" color="primary" onClick={btnClickHandler}>
         Import Data
       </Button><br />
-      <Button variant="outlined" color="primary" className={classes.btn2} onClick={btnClickHandler}>
+      <Button variant="outlined" color="secondary" onClick={btnClickHandler}>
         Lookup Data
       </Button>
     </PageContainer>
