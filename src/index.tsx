acf88267c15f812cc399f8680503633ccec9ed66
Nimesh Solanki <nimesh3536@gmail.com>
2020-09-21 18:26:18 +0530

    use app component as root component

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
-import SelectSourcePage from './SelectSourcePage';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <SelectSourcePage />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
